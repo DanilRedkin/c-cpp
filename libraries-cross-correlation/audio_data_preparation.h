c717cee52d6ddedee5f00daa250470a98729d6af
elyfrucial <mr.dredkin@gmail.com>
2025-04-10 23:38:52 +0300

    new

--- libraries-cross-correlation/audio_data_preparation.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-int prepare_audio_data(int argc, char **argv, double **prepared_data1, double **prepared_data2, size_t *data1_len, size_t *data2_len, size_t *sample_rate);
